0c745a6193d21ab487a7bd4753927c22b40b3886
Jared <jkenton21@live.com>
2022-03-01 17:50:30 -0600

    initial JS file created and objects created for Player X, Player Y, and the Game board

---
+++ script.js
@@ -0,0 +1,19 @@
+// Tic-Tac-Toe Game -- JavaScript file //
+
+//Gameboard object
+function gameBoard(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9) {
+    game: [cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9]
+    return game;
+};
+
+
+//Player object
+function playerX(nameX) {
+    nameX: nameX
+    return nameX;
+}
+
+function playerO(nameO) {
+    nameO: nameO
+    return nameO;
+}
